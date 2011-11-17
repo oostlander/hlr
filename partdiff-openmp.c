cf37f7b8238e9258ebb63d457a083b61c9424597
Marvin <marvinvollcrass@web.de>
2011-11-17 16:26:53 +0100

    Makefile hinzuegefuegt partdiff-openmp* erstellt

---
+++ partdiff-openmp.c
@@ -0,0 +1,13 @@
+// Hello World Prog
+
+// libs
+#include <stdio.h>
+
+// Main-Funktion
+int main()
+{ 
+  // "Hello World" mit Zeilenumbruch wird ausgegeben
+  printf("Hello World\n");
+
+  return 0; // return 0 tuttifruiti return 1 remmidemmi
+}
