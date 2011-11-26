72a24882f95962d0d824f3a95cb62e08519ecdd8
Klaas Oostlander <klaas.oostlander@googlemail.com>
2011-11-26 20:51:02 +0100

    Added a Makefile and some comments.

--- timempi/timempi.c
+++ timempi/timempi.c
@@ -7,6 +7,7 @@ char *argv[];
 {
 	int numtasks, rank, rc;
 	
+	/* initialize MPI and check for success*/
 	rc = MPI_Init(&argc,&argv);
 	if (rc != MPI_SUCCESS)
 	{
@@ -14,6 +15,7 @@ char *argv[];
 		MPI_Abort(MPI_COMM_WORLD, rc);
 	}
 	
+	/* get size of comm and rank in that comm */
 	MPI_Comm_size(MPI_COMM_WORLD,&numtasks );
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	printf ("Number of tasks= %d Myrank= %d\n", numtasks,rank);
