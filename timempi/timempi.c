ab6ba486b7f830512c52cb35d9f821549879cb74
Klaas Oostlander <klaas.oostlander@googlemail.com>
2011-11-27 19:13:22 +0100

    Versenden und empfangen von timestamps funktioniert in korrkter Reihenfolge.

--- timempi/timempi.c
+++ timempi/timempi.c
@@ -21,7 +21,7 @@ int main(int argc,char* argv[])
 	//printf ("Number of tasks= %d Myrank= %d\n", numtasks,rank);
 	
 	// We are assuming at least 2 processes for this task
-	if (numtasks != 2) 
+	if (numtasks < 2) 
 	  {
 	    fprintf(stderr, "World size must be at least two for %s to run properly!\n", argv[0]);
 	    MPI_Abort(MPI_COMM_WORLD, 1); 
@@ -31,31 +31,38 @@ int main(int argc,char* argv[])
 	int resultlength=0;
 	int number;
 	MPI_Get_processor_name(hostname,&resultlength);
-
-	/* get current time */
-	struct tm *Tm;
-	struct timeval detail_time;
-	time_t timer = time(NULL);
-	Tm=localtime(&timer);
-	gettimeofday(&detail_time,NULL);
-	char timestamp[60];
-	snprintf(timestamp,60,"%d %d %d, %d:%d:%d and %dms\n",
-	       //Tm->tm_wday, /* Mon - Sun */
-	       Tm->tm_mday,
-	       Tm->tm_mon+1,
-	       Tm->tm_year+1900,
-	       Tm->tm_hour,
-	       Tm->tm_min,
-	       Tm->tm_sec,
-	       (int) detail_time.tv_usec /1000);
-	if (rank == 0)
+	if (rank != 0)
 	  {
-	    number=-1;
-	    MPI_Send(timestamp, 60, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
-	  } else if (rank == 1)
+	    /* get current time */
+	    struct tm *Tm;
+	    struct timeval detail_time;
+	    time_t timer = time(NULL);
+	    Tm=localtime(&timer);
+	    gettimeofday(&detail_time,NULL);
+	    char timestamp[60];
+	    snprintf(timestamp,60,"%s(%d):%d %d %d, %d:%d:%d and %dms\n",
+		     //Tm->tm_wday, /* Mon - Sun */
+		     hostname,
+		     rank,
+		     Tm->tm_mday,
+		     Tm->tm_mon+1,
+		     Tm->tm_year+1900,
+		     Tm->tm_hour,
+		     Tm->tm_min,
+		     Tm->tm_sec,
+		     (int) detail_time.tv_usec /1000);
+	    /* send timestamp to Master */
+	    MPI_Send(timestamp, 60, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+	  }else if (rank == 0)
 	  {
-	    MPI_Recv(timestamp, 60, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	    printf("Process 1 recieved number %s from process o\n",timestamp);
+	    /* print recieved Messages */
+	    int i;
+	    for (i = 1; i < numtasks; i++)
+	      {
+		char buf[60];
+		MPI_Recv(buf, 60, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		fprintf(stdout, "%s", buf);
+	      }
 	  }
 	/****** do some work ******/
 
