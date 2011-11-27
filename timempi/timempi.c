a932c012bf0173e203784e3bd251869d4e3beef7
Klaas Oostlander <klaas.oostlander@googlemail.com>
2011-11-27 17:36:32 +0100

    timempi etwas erweitert und Makefile angepasst (temporär).

--- timempi/timempi.c
+++ timempi/timempi.c
@@ -1,9 +1,8 @@
-#include "mpi.h"
+#include <mpi.h>
 #include <stdio.h>
+#include <time.h>
 
-int main(argc,argv)
-int argc;
-char *argv[];
+int main(int argc,char* argv[])
 {
 	int numtasks, rank, rc;
 	
@@ -18,9 +17,31 @@ char *argv[];
 	/* get size of comm and rank in that comm */
 	MPI_Comm_size(MPI_COMM_WORLD,&numtasks );
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-	printf ("Number of tasks= %d Myrank= %d\n", numtasks,rank);
+	//printf ("Number of tasks= %d Myrank= %d\n", numtasks,rank);
 	
+	// We are assuming at least 2 processes for this task
+	if (numtasks != 2) 
+	  {
+	    fprintf(stderr, "World size must be at least two for %s to run properly!\n", argv[0]);
+	    MPI_Abort(MPI_COMM_WORLD, 1); 
+	  }
+	int namelength=MPI_MAX_PROCESSOR_NAME;
+	char hostname[namelength];
+	int resultlength=0;
+	int number;
+	MPI_Get_processor_name(hostname,&resultlength);
+	time_t timer = time(NULL);
+	if (rank == 0)
+	  {
+	    number=-1;
+	    MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+	  } else if (rank == 1)
+	  {
+	    MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	    printf("Process 1 recieved number %d from process o\n",number);
+	  }
 	/****** do some work ******/
-	
+
+	/* finalize the MPI environment */
 	MPI_Finalize();
 }
